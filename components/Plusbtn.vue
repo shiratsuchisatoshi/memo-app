456956fc9a304fc0c810412bbd9d0c5bd53efe3b
shiratsuchisatoshi <white.earth.3104@gmail.com>
2019-01-26 08:23:22 +0900

    a

---
+++ components/Plusbtn.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="plusbtn" @click="$emit('plus')">
+    +
+  </div>
+</template>
+
+<style>
+/* .plusbtn {
+   position: absolute;
+   bottom: 10px;
+   right: 10px;
+   width: 50px;
+   height: 50px;
+   background: white;
+   border-radius: 50px;
+   font-size: 30px;
+   cursor: pointer;
+} */
+.plusbtn {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  background: white;
+  border-radius: 50px;
+  font-size: 30px;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 10px 20px;
+  border-bottom: 4px solid white;
+  cursor: pointer;
+}
+
+.plusbtn:active {
+    transform: translateY(4px);
+    border-bottom: none;
+}
+</style>
